92a556c4a966f999eae9490b978e14f55c5d7a40
Hamza Saddouki <hsaddouki@gmail.com>
2017-12-11 15:07:39 +0100

    Add project files.

---
+++ Simple form/Controllers/CalculController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simple_form.Controllers
+{
+    public class CalculController : Controller
+    {
+        // GET: Calcul
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("suma"), HttpGet]
+        public ActionResult Suma()
+        {
+            return View();
+        }
+
+        [Route("suma"), HttpPost]
+        public ActionResult Suma(int numero1, int numero2)
+        {
+
+            ViewBag.Resultat = string.Format("El resultat de {0} + {1} = {2}",
+                                              numero1, numero2, numero1 + numero2);
+            return View();
+        }
+    }
+}
